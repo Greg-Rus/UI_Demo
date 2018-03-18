f3945c2ba6543f7f267d5cec0ad8c57d67404960
Greg-Rus <grzegorz.rus@gmail.com>
2018-03-19 00:03:52 +0100

    New Project

---
+++ UI Demo/Assets/ParentingDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentingDemo : MonoBehaviour
+{
+
+    public RectTransform PanelOne;
+    public RectTransform PanelTwo;
+
+    private RectTransform _myRect;
+	// Use this for initialization
+	void Start ()
+	{
+	    _myRect = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+
+    public void SetParentWorldStays()
+    {
+        _myRect.SetParent(_myRect.parent == PanelOne ? PanelTwo : PanelOne, true);
+    }
+
+    public void SetParentLocalStays()
+    {
+        _myRect.SetParent(_myRect.parent == PanelOne ? PanelTwo : PanelOne, false);
+    }
+
+    public void SetParentLegacy()
+    {
+        _myRect.SetParent(_myRect.parent == PanelOne ? PanelTwo : PanelOne);
+    }
+}
