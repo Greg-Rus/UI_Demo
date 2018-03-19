f9d79745d7dec21a864aa5474856f0b145c39a27
Greg-Rus <grzegorz.rus@gmail.com>
2018-03-20 00:13:11 +0100

    Movement Demo start

---
+++ UI Demo/Assets/InsetDemo.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class InsetDemo : MonoBehaviour
+{
+    private RectTransform _myRectTransform;
+    void Start ()
+    {
+        _myRectTransform = GetComponent<RectTransform>();
+    }
+
+	//void Update () {
+		
+	//}
+
+    public void InsetHorisontal(float normalizedInset)
+    {
+        var x = _myRectTransform.parent as RectTransform;
+        _myRectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, x.rect.width * normalizedInset, _myRectTransform.rect.width);
+    }
+    public void InsetVertical(float normalizedInset)
+    {
+        var x = _myRectTransform.parent as RectTransform;
+        _myRectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, x.rect.height * normalizedInset, _myRectTransform.rect.height);
+    }
+}
---
+++ UI Demo/Assets/RectMovment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RectMovment : MonoBehaviour
+{
+    public float speed = 1;
+    private RectTransform _myRectTransform;
+	// Use this for initialization
+	void Start ()
+	{
+	    _myRectTransform = GetComponent<RectTransform>();
+
+	    
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        _myRectTransform.anchoredPosition += Vector2.up * Input.GetAxis("Vertical") * speed;
+	    _myRectTransform.anchoredPosition += Vector2.right * Input.GetAxis("Horizontal") * speed;
+
+	    
+	}
+}
