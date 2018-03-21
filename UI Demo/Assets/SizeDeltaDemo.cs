08fbb75405f06394e39f9d13d67ac3c6d1ef51ae
Greg-Rus <grzegorz.rus@gmail.com>
2018-03-21 23:08:43 +0100

    Size Delta demo

---
+++ UI Demo/Assets/SizeDeltaDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeDeltaDemo : MonoBehaviour {
+    private RectTransform _myRectTransform;
+
+    public Vector2 SizeDelta;
+    // Use this for initialization
+    void Start()
+    {
+        _myRectTransform = GetComponent<RectTransform>();
+    }
+
+    void OnValidate()
+    {
+        SetOffset();
+    }
+
+    // Update is called once per frame
+    void SetOffset()
+    {
+        _myRectTransform.sizeDelta = SizeDelta;
+
+    }
+
+}
